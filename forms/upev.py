c05ce1f0e5b9adf8d6bde450659000ecdd4344bb
victor <victorcalehr18@gmail.com>
2025-11-08 23:02:21 -0600

    main functionality

--- forms/upev.py
+++ forms/upev.py
@@ -4,5 +4,19 @@ import random
 
 time.sleep(2)
 
+a=1
 while True:
     pyautogui.press("tab", 3)
+    pyautogui.press("enter")
+    pyautogui.press("down",a)
+    a+=1
+    if a>51:
+        a=1
+    pyautogui.press("enter")
+    pyautogui.press("tab")
+    # 20 or 28
+    pyautogui.press("down",5)
+    pyautogui.press("tab")
+    pyautogui.press("down",4)
+    pyautogui.press("tab")
+    pyautogui.press("down",2)
