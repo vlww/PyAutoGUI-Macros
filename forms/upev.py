0af851d1d93ffd2b831095eb349abb6470f767d1
victor <victorcalehr18@gmail.com>
2025-11-08 23:07:52 -0600

    q1

--- forms/upev.py
+++ forms/upev.py
@@ -15,7 +15,15 @@ while True:
     pyautogui.press("enter")
     pyautogui.press("tab")
     # 20 or 28
-    pyautogui.press("down",5)
+    if a==20:
+        if (int(random.random()*2)+1) == 1:
+            pyautogui.press("down",1)
+        else:
+            pyautogui.press("down",6)
+    elif a==28:
+        pyautogui.press("down",int(random.random()*3)+2)
+    else:
+        pyautogui.press("down",5)
     pyautogui.press("tab")
     pyautogui.press("down",4)
     pyautogui.press("tab")
