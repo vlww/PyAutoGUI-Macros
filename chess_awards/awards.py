543990a98748c49043e15369cb25e7cbbef69dfd
victor <victorcalehr18@gmail.com>
2026-07-27 19:36:04 +0900

    changed positions

--- chess_awards/awards.py
+++ chess_awards/awards.py
@@ -2,17 +2,26 @@ import pyautogui
 import time
 import random
 
-# edge on 732
+# edge on 730
 refresh_interval = 600 #10min
 start_time = time.time()
 
 time.sleep(2)
+
 positions = [
-    [225, 470], [225, 625],
-    [440, 470], [335, 625],
-    [335, 470], [440, 625],
-    [550, 470], [550, 625]
+    [185, 450], [185, 610],
+    [415, 450], [300, 610],
+    [300, 450], [415, 610],
+    [530, 450], [530, 610]
 ]
+
+#positions = [
+#    [225, 470], [225, 625],
+#    [440, 470], [335, 625],
+#    [335, 470], [440, 625],
+#    [550, 470], [550, 625]
+#]
+
 
 
 while True:
@@ -23,17 +32,17 @@ while True:
         time.sleep(3) 
         start_time = time.time()
 
-    pyautogui.click(720,220)
+    pyautogui.click(430, 335) #720,220
     time.sleep(0.1)
-    pyautogui.click(600,365)
+    pyautogui.click(300, 485) #600,365
     time.sleep(0.3)
     num = random.randint(0, 7)
     pyautogui.click(positions[num][0],positions[num][1])
     if (num<=3):
-        pyautogui.click(400,520)
+        pyautogui.click(365, 515) #400,520
     else:
-        pyautogui.click(400,500)
-    time.sleep(0.5)
+        pyautogui.click(365, 495) #400,500
+    time.sleep(0.7)
     pyautogui.click(410,280)
     time.sleep(0.1)
 
