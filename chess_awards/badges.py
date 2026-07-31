0f1598663821c419dcc5c2b7a6467c6ca7fc340f
victor <victorcalehr18@gmail.com>
2026-08-01 01:49:44 +0900

    increased cooldown

--- chess_awards/badges.py
+++ chess_awards/badges.py
@@ -40,6 +40,6 @@ while True:
         pyautogui.click(460, 495) #small
     else:
         pyautogui.click(460, 515) #big
-    time.sleep(1.3)
+    time.sleep(1.5)
     pyautogui.click(485,275)
     time.sleep(0.1)
