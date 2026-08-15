29e8eda2cb08f8c6514784e0708adf685724e3a0
victor <victorcalehr18@gmail.com>
2026-08-15 01:29:36 -0500

    reverted changes

--- chess_awards/badges.py
+++ chess_awards/badges.py
@@ -29,7 +29,7 @@ while True:
         time.sleep(3)
 
     pyautogui.click(745, 530) 
-    time.sleep(0.8)
+    time.sleep(0.9)
     num = random.randint(0, 8) #extra for weighting
     pyautogui.click(positions[num][0],positions[num][1])
     if (num<=3):
